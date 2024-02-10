35ee03050a44f91131127df90c7382ba1e45f16a
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-02-10 16:40:12 -0300

    primeiro comit 5 desafio da alura

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+function desafio_1(){
+    console.log('fui chamado desafio 1');
+    alert('fui chamado desafio 1');
+};
+
+function desafio_2(){
+    console.log('fui chamado desafio 2');
+    alert('fui chamado desafio 2');
+};
+
+function desafio_3(){
+    console.log('fui chamado desafio 3');
+    alert('fui chamado desafio 3');
+};
+
+function desafio_4(){
+    console.log('fui chamado desafio 4');
+    alert('fui chamado desafio 4');
+};
+
+function desafio_5(){
+    console.log('fui chamado desafio 5');
+    alert('fui chamado desafio 5');
+};
