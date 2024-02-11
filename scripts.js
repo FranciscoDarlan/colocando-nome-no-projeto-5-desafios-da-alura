878bd8fb3677f6409b5a17db36209f1676582864
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-02-11 13:14:39 -0300

    resolucao desafio1

--- scripts.js
+++ scripts.js
@@ -1,29 +1,36 @@
 
-function desafio_1(){
-    let diasDasemana = [ 'segunda', 'terça', 'quarta', 'quinta', 'sexta'];
-    let diasFinaisSemana = [ 'sabado', 'domingo'];
-    console.log(diasDasemana)
-    console.log(diasFinaisSemana)
+function desafio_1() {
 
     alert('desafio 1 vai ser iniciado!');
-   let diaDaSemana = prompt('QUAL DIA DA SEMANA VOCÊ ESTÁ?');
+    let diaDaSemana = prompt('QUAL DIA DA SEMANA VOCÊ ESTÁ?');
     console.log(diaDaSemana)
 
-   if (diaDaSemana === 'domingo'||'sabado'){
+    if (diaDaSemana === 'domingo' || 'sabado') {
         alert('BOM FIM DE SEMANA!')
-        console.log(diaDaSemana === 'domingo'||'sabado')
-   } else{
-    alert('BOA SEMANA!')
-   }
+        console.log(diaDaSemana === 'domingo' || 'sabado')
+    } else {
+        alert('BOA SEMANA!')
+    }
+
+
+    // RESPOSTA DO PROFESSSOR: 
+    //  diaDaSemana = prompt('Qual é o dia da semana?');
+    //  if (diaDaSemana == 'Sábado') {
+    //   alert('Bom fim de semana!');
+    //}  else if (diaDaSemana == 'Domingo') {
+    //    alert('Bom fim de semana!');
+    //}    else {
+    //    alert('Boa semana!');
+    //}
 };
 
-function desafio_2(){
-    
+function desafio_2() {
+
     alert('VAI INICIAR O DESAFIO 2');
     alert('IREI FALAR SE SEU NÚMERO É POSITVO OU NEGATIVO ..');
     let numeroDigitado = prompt('DIGITE UM NÚMERO "POSITIVO" OU "NEGATIVO" ...');
 
-    if ( numeroDigitado > 0) {
+    if (numeroDigitado > 0) {
         alert('SEU NÚMERO É POSITVO!!!')
     }
 
@@ -32,8 +39,8 @@ function desafio_2(){
     }
 };
 
-function desafio_3(){
-    
+function desafio_3() {
+
     alert('VAI INICIAR O DESAFIO 3');
     alert('FALE UM NÚMERO DE 1 A 1000 !!!!');
     let score = prompt('O NÚMERO QUE VOCÊ FALOU É A QUANTIDADE DE PONTOS QUE VOCÊ FEZ NO JOGO! QUAL NÚMERO QUE VOCÊ FALOU? ');
@@ -41,20 +48,20 @@ function desafio_3(){
     if (score >= 100) {
         alert('Parabéns, você venceu!');
 
-    }else{
+    } else {
         alert('Tente novamente para ganhar.');
     }
 };
 
-function desafio_4(){
-    
+function desafio_4() {
+
     alert('VAI INICIAR O DESAFIO 4');
     let saldo = prompt('QUANTO VOCÊ TEM NA CONTA ??')
     alert(` SEU SALDO NA CONTA É DE ${saldo} REAIS.`)
 };
 
-function desafio_5(){
-    
+function desafio_5() {
+
     let nome = prompt('QUAL SEU NOME?')
     alert(`OLÁ ${nome}! SEJA BEM VINDO  AO DESAFIO 5`);
 };
